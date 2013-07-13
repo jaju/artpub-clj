1da863602e25bdd0d9688e87aeb6764c79f1f1f7
Ravindra R. Jaju <jaju@msync.org>
2013-07-13 07:09:21 +0530

    First check for HTML, and then use md.

--- src/artpub/routes/home.clj
+++ src/artpub/routes/home.clj
@@ -1,15 +1,21 @@
 (ns artpub.routes.home
   (:use compojure.core)
   (:require [artpub.views.layout :as layout]
-            [artpub.util :as util]))
+            [artpub.util :as util]
+            [clojure.java.io :as io]))
 
 (defn home-page []
   (layout/render
    "home.html" {:content (util/md->html "/md/docs.md")}))
 
+(defn article-html-and-then-md [name]
+  (if-let [f (io/resource (str "public/articles-html/" name ".html"))]
+    (slurp f)
+    (util/md->html (str "/md/" name ".md"))))
+
 (defn article [name]
   (layout/render
-   "home.html" {:content (util/md->html (str "/md/" name ".md"))}))
+   "home.html" {:content (article-html-and-then-md name)}))
 
 (defn about-page []
   (layout/render "about.html"))
