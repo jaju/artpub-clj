9ae8422ec78322fbe8b6539a7bd26840f62da06c
Ravindra R. Jaju <jaju@msync.org>
2013-07-13 06:58:18 +0530

    Display user's first name when available.

--- src/artpub/views/layout.clj
+++ src/artpub/views/layout.clj
@@ -1,12 +1,20 @@
 (ns artpub.views.layout
   (:use noir.request)
   (:require [clabango.parser :as parser]
-            [noir.session :as session]))
+            [noir.session :as session]
+            [artpub.models.db :as db]))
 
 (def template-path "artpub/views/templates/")
+
+(defn user-display-name [id]
+  (let [user (db/get-user id)]
+    (if-let [display (:first_name user)]
+      display
+      (str "(" id ")"))))
 
 (defn render [template & [params]]
   (parser/render-file (str template-path template)
                       (assoc (or params {})
                         :context (:context *request*)
-                        :user-id (session/get :user-id))))
+                        :user-id (session/get :user-id)
+                        :user-name (user-display-name (session/get :user-id)))))
