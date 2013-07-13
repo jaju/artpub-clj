bd6ac86e847196a8d6f3cc381a47897c466dc91d
Ravindra R. Jaju <jaju@msync.org>
2013-07-13 11:43:48 +0530

    Updated cljs file to populate navigation panel on load, by searching for section headers.

--- src-cljs/main.cljs
+++ src-cljs/main.cljs
@@ -2,7 +2,11 @@
   (:require [ajax.core :refer [GET POST]] 
             [domina :refer [value by-id destroy-children! append!]]
             [domina.events :refer [listen!]]
-            [dommy.template :as template]))
+            [dommy.core :as dommy]
+            [dommy.attrs :as attrs]
+            [dommy.template :as template])
+  (:use-macros
+   [dommy.macros :only [sel sel1 node]]))
 
 (defn render-message [{:keys [message user]}]
   [:li [:p {:id user} message " - " user]])
@@ -22,6 +26,24 @@
                   :user    (value (by-id "user"))}
          :handler render-messages}))
 
+(defn get-sections []
+  (sel [:#content :section]))
+
+(defn article-nav-node []  (sel1 [:#left-sidenav :.sidenav]))
+
+(defn create-section-nav-elem [section]
+  (let [header (sel1 section [:header])
+        text (dommy/text header)
+        href (str "#" (attrs/attr section :id))]
+    (node [:li [:a {:href href} text]])))
+
+(defn populate-article-navigator []
+  (doseq [section (get-sections)]
+    (dommy/append! (article-nav-node) (create-section-nav-elem section))))
+
 (defn ^:export init []
   (GET "/messages" {:handler render-messages})
-  (listen! (by-id "send") :click add-message))
+  (listen! (by-id "send") :click add-message)
+  (populate-article-navigator))
+
+(set! (.-onload js/window) init)
